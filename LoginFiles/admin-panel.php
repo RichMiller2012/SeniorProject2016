7001df88c2118b75ff868372cad2ef294aa99b16
Richard Miller <Richard.Miller@healthsparq.com>
2016-10-07 14:49:44 -0700

    Added logout functionality. Login/Logout functionality complete

--- LoginFiles/admin-panel.php
+++ LoginFiles/admin-panel.php
@@ -1,1 +1,31 @@
 <h1>Admin Panel</h1>
+
+<?php
+
+	/*
+		begin adding all of the php includes for the different
+		scenarios of administrative tasks
+
+		Add a root category
+		Add a subcategory
+		Add an article to a category/subcategory
+		
+		Remove a category (warn user that all sub-data will be lost)
+		Remove a sub category (same warning)
+		Remove an article
+		
+		Edit a root category
+		Edit a subcategory
+		Edit and article
+		
+		Reattach a subcategory to another category
+		Attach a root category to another category/subcategory
+		Reattach an article to another category
+
+		Update "whats new" section
+	
+	*/
+
+?>
+
+<h4><a href="adminindex.php?logout=true">Logout</a></h4>
--- LoginFiles/adminindex.php
+++ LoginFiles/adminindex.php
@@ -2,22 +2,32 @@
 
 	include("dbconnect.php");
 
+	//start the session
 	session_start();
 	
+	//check if user has logged out, clear the session if so
+	if(isset($_GET['logout'])) {
+		unset($_SESSION['admin']);
+	}
+	
+	//check if user has clicked the logout button
 	if(isset($_POST['login'])){
 
+		//query the database with users name and password
 		$login_sql="SELECT * FROM user WHERE username='" . $_POST['username'] . "' AND password='" . sha1($_POST['password']) . "'";
 		$login_query=mysqli_query($dbconnect, $login_sql);
 		
+		//check if a result is returned
 		if(mysqli_num_rows($login_query) > 0) {
 			$login_rs=mysqli_fetch_assoc($login_query);
 			$_SESSION['admin']=$login_rs['username'];
 		} else {
-			echo "      " . sha1($_POST['password']);
-			//header("location:adminindex.php?error=login");
+			//if no result is returned, go back to login form
+			header("location:adminindex.php?error=login");
 		}
 	}
 
+	//if session is not set, go to login form, else go to admin panel
 	if(!isset($_SESSION['admin'])){
 	    include("login.php");
 	} else {    
