1d0a6e9da23b2b8ab2dba43cb9dbb1e5eef42680
rmiller2 <rich.miller2012@gmail.com>
2017-01-22 19:12:19 -0800

    Finished adding star rating

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="css/styles.css">
   <link rel="stylesheet" type="text/css" href="css/autosuggest-style.css">
+  <link rel="stylesheet" type="text/css" href="css/rating.css">
 </head>
 <body>
 
