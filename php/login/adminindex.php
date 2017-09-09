8a77e5a431de685d3a5c4850ff47b3ffe30ca83a
Richard <rich.miller2012@gmail.com>
2017-09-09 02:00:57 -0400

    fixing output flushing on server side

--- php/login/adminindex.php
+++ php/login/adminindex.php
@@ -1,11 +1,10 @@
-<h1>ADMIN INDEX</h1>
 <?php 
 
 	include("dbconnect.php");
 
 	//start the session
 	session_start();
-	
+
 	//check if user has logged out, clear the session if so
 	if(isset($_GET['logout'])) {
 		unset($_SESSION['admin']);
@@ -26,8 +25,6 @@
 		$login_sql="SELECT * FROM user WHERE username='" . $_POST['username'] . "' AND password='" . sha1($_POST['password']) . "'";
 		$login_query=mysqli_query($dbconnect, $login_sql);
 		
-		echo $login_sql;
-		
 		//check to see if a result is returned
 		if(mysqli_num_rows($login_query) > 0) {
 			echo "login success";
@@ -35,7 +32,6 @@
 			$_SESSION['admin']=$login_rs['username'];
 			header("location: admin-panel.php");
 		} else {		
-			//if no result is returned, go back to login form
 			header("location: login.php?error=login");
 		}
 	}
