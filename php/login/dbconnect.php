99a2439d8a9b17f8c102ee14ecb4fb4276ca72fb
rmiller2 <rich.miller2012@gmail.com>
2017-09-08 22:07:10 -0700

    adding remote db connection info conditional for admin section

--- php/login/dbconnect.php
+++ php/login/dbconnect.php
@@ -1,5 +1,12 @@
 <?php
 
-	$dbconnect = mysqli_connect("localhost", "root", "", "hsdb")
+	//try local, then try remote server
+  $dbconnect = mysqli_connect("localhost", "root", "", "hsdb");
+  if(mysqli_connect_errno()){
+	  $dbconnect = mysqli_connect("localhost", "tfamilyh_test", "thefamilyhomestead2017", "tfamilyh_test");
+  } else {
+	 echo "Connection failed:".mysqli_connect_error();
+	  exit; 
+  }
 
 ?>
