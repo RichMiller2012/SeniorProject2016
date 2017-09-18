847e463e37ecad70da27c8df48f906ff574187f0
rmiller2 <rich.miller2012@gmail.com>
2017-09-17 20:38:33 -0700

    fixing dbconnect files again

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,8 +1,7 @@
 <?php
-  //$dbconnect = mysqli_connect("localhost", "tfamilyh_test", "thefamilyhomestead2017", "tfamilyh_test");
-  $dbconnect = mysqli_connect("localhost", "root", "", "hsdb");
+  $dbconnect = mysqli_connect("localhost", "tfamilyh_test", "thefamilyhomestead2017", "tfamilyh_test");
   if(mysqli_connect_errno()){
-	  
+	  $dbconnect = mysqli_connect("localhost", "root", "", "hsdb");
 	  if(mysqli_connect_errno()){
 		  echo "Connection failed:".mysqli_connect_error();
 	  exit;
